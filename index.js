ed39473dab231e270fb00f10200a2af670aec414
juaneraso <juangui9508@hotmail.com>
2024-10-27 21:37:44 -0500

    fix routes

--- index.js
+++ index.js
@@ -147,14 +147,14 @@ app.post("/chat", async (req, res) => {
         {
           text: "Please my dear, don't forget to add your API keys!",
           audio: await audioFileToBase64("audios/api_0.wav"),
-          lipsync: await readJsonTranscript("/home/app/audios/api_0.json"),
+          lipsync: await readJsonTranscript("audios/api_0.json"),
           facialExpression: "angry",
           animation: "Angry",
         },
         {
           text: "You don't want to ruin Wawa Sensei with a crazy ChatGPT and ElevenLabs bill, right?",
           audio: await audioFileToBase64("audios/api_1.wav"),
-          lipsync: await readJsonTranscript("/home/app/audios/api_1.json"),
+          lipsync: await readJsonTranscript("audios/api_1.json"),
           facialExpression: "smile",
           animation: "Laughing",
         },
